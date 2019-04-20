847c7216f6517d60285249fd3a1776a268d5c431
Ssadaf <s.sadaf2277@gmail.com>
2019-04-20 11:23:44 +0430

    This commit will add constructor and source and destination port num to packet class

--- codes/Packet.java
+++ codes/Packet.java
@@ -1,11 +1,21 @@
 import java.io.*; 
+
+
   
 class Packet implements java.io.Serializable 
 {  
     private String ack_flag;
     private String syn_flag;
-    private String source_port;
-    private String destination_port;
+    private static final String source_port = "1111";
+    private static final String destination_port = "7777";
     private String ack_number;
     private String seq_number;
+
+    public Packet(String ack_flag, String syn_flag, String ack_number, String data)
+    {        
+        this.ack_flag = ack_flag;
+        this.syn_flag = syn_flag;
+        this.ack_number = ack_number;
+        this.data = data;
+    }
 }
