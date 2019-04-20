bd10a4ff66f93d7520f45409f75ec9392d974e55
Yasaman <ys.jafari98@gmail.com>
2019-04-20 11:18:39 +0430

    This commit will add getters.

--- codes/Packet.java
+++ codes/Packet.java
@@ -7,5 +7,21 @@ class Packet implements java.io.Serializable
     private String source_port;
     private String destination_port;
     private String ack_number;
-    private String seq_number;
+    private String data;
+
+    public String get_ack_flag(){
+        return ack_flag;
+    }
+
+    public String get_syn_flag(){
+        return syn_flag;
+    }
+
+    public String get_ack_number(){
+        return ack_number;
+    }
+
+    public String get_data(){
+        return data;
+    }
 }
