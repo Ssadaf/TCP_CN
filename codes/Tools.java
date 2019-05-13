5d5fe81e5ffb3a577534ec0e476b0a34488f1f8a
Yasaman <ys.jafari98@gmail.com>
2019-05-13 17:52:20 +0430

    This commit will delete null bytes at the end of data

--- codes/Tools.java
+++ codes/Tools.java
@@ -1,15 +1,22 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Tools {
     static public byte[] concatenate(byte[] a, byte[] b) {
         int aLen = a.length;
         int bLen = b.length;
 
-        @SuppressWarnings("unchecked")
         byte[] c = (byte[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
         System.arraycopy(a, 0, c, 0, aLen);
         System.arraycopy(b, 0, c, aLen, bLen);
 
         return c;
+    }
+
+    static public byte[] deleteNullBytes(byte[] input){
+        int i = input.length - 1;
+        while (i >= 0 && input[i] == 0)
+            --i;
+        return Arrays.copyOf(input, i + 1);
     }
 }
